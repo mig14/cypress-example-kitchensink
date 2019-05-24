0efd15bad5ef5ed5a3f1c66ab894f7d56e0e2c72
mig14 <mig14@ic.ac.uk>
2019-05-24 10:04:34 +0100

    test add file

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+describe('Sample test group', function() {
+    describe('Nested test group', function() {
+        it('Sample test 1: Does not do much!', function() {
+            expect(true).to.equal(true)
+          });
+          it('Sample test 2: should return -1 when the value is not present', function(){
+              assert.equal(-1, [1,2,3].indexOf(4));
+            });
+
+    });
+  });
+
+  describe('My first test', function(){
+      it('visits Kitchen Sink', function() {
+          cy.visit('https://example.cypress.io');
+      })
+  });
